b21850045e50a1c1c1395056be0a41db06cf7aec
Michael Emmanuel <mike@Michaels-MacBook-Pro-3.local>
2023-08-28 23:03:38 -0400

    game loop and player turns set up

--- index.js
+++ index.js
@@ -27,9 +27,7 @@ let playerOMoves = [
 async function startGame() {
   while (!gameIsOver) {
     console.log();
-
     drawGrid(playerXMoves, playerOMoves);
-
     console.log();
 
     let response = await displayPrompt(`${currentPlayer}, please enter your next move: `)
@@ -47,16 +45,20 @@ async function startGame() {
   }
 }
 
-function drawGrid() {
-  drawVerticalLines();
+function drawGrid(xMoves, oMoves) {
+  drawVerticalLines(xMoves[0], oMoves[0]);
   drawHorizontalLine();
-  drawVerticalLines();
+  drawVerticalLines(xMoves[1], oMoves[1]);
   drawHorizontalLine();
-  drawVerticalLines();
+  drawVerticalLines(xMoves[2], oMoves[2]);
 }
 
-function drawVerticalLines() {
-  console.log('   |   |   ');
+function drawVerticalLines(xMoves, oMoves) {
+  let space1Char = xMoves[0] ? 'X' : oMoves[0] ? 'O' : ' ';
+  let space2Char = xMoves[1] ? 'X' : oMoves[1] ? 'O' : ' ';
+  let space3Char = xMoves[2] ? 'X' : oMoves[2] ? 'O' : ' ';
+
+  console.log(` ${space1Char} | ${space2Char} | ${space3Char} `);
 }
 
 function drawHorizontalLine() {
